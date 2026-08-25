572b32801fefbfaeff0e037b1109d42d7bdda87c
Navami <navamic797@gmail.com>
2026-08-25 12:20:02 +0530

    Add first c program

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.stdio> // Include standard input-output library
+
+int main() {
+    // Print Hello, World! to the console
+    printf("Hello, World!\n");
+    return 0; // Signal successful execution
+}
